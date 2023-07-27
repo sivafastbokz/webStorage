176355aaed19969fc4cb34658dfa7b9eacce64df
sivafastbokz <sivaharshanfastbokz@gmail.com>
2023-07-27 12:14:46 +0530

    some updates pending

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const[userName,setUserName]=useState('');
+  const[userAge,setUserAge]=useState('');
+  const[name,setName]=useState('');
+  const[age,setAge]=useState('');
+ 
+   const handleSubmit=()=>{
+    localStorage.setItem('name',userName);
+    localStorage.setItem('age',userAge);
+    sessionStorage.setItem('name',userName);
+    sessionStorage.setItem('age',userAge);
+    document.cookie=('name',userName)
+   }
+  
+   const handleGet=()=>{
+    setName(localStorage.getItem('name'));
+    setAge(localStorage.getItem('age'))
+   }
+
+   const handleClear=()=>{
+    localStorage.clear()
+   }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <React.Fragment>
+    <input type='text' placeholder='username' onChange={(event)=>setUserName(event.target.value)}></input>
+    <br/>
+    <input type='number' placeholder='userage' onChange={(event)=>setUserAge(event.target.value)}></input>
+    <br/>
+    <button onClick={handleSubmit}>sumbit</button>
+    <button onClick={handleGet}>get userData</button>
+    <button onClick={handleClear}>clear</button>
+    <table>
+      <tr>
+          <th>name</th>
+          <th>age</th>
+      </tr>
+      <tr>
+        <td>{name}</td>
+        <td>{age}</td>
+      </tr>
+
+    </table>
+   </React.Fragment>
   );
 }
 
